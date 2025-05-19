bea97ec701275158499385a6768554a3b3776cb1
crestfall <mas025@edu.zealand.dk>
2025-05-19 12:28:54 +0200

    added funtions that work in program

--- ConsoleAppDyr/Program.cs
+++ ConsoleAppDyr/Program.cs
@@ -1,5 +1,8 @@
 ﻿using DyrinternatLibarary.Model;
+using DyrinternatLibarary.Repository;
 using DyrinternatLibarary.Service;
+
+
 using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleAppDyr
@@ -8,27 +11,34 @@ namespace ConsoleAppDyr
     {
         static void Main(string[] args)
         {
-            AnimalService _aniser;
 
+
+           // AnimalService _aniser = new AnimalService();
+
+            AnimalRepo _aniro = new AnimalRepo();
             Animal dwedw = new Animal();
             Animal edwdw = new Animal();
             Animal fefe = new Animal();
 
             List<Animal> anis = new List<Animal>();
+            
+            
+
+            anis = _aniro.GetAll();
             anis.Add(dwedw);
             anis.Add(edwdw);
             anis.Add(fefe);
-            
-           
 
+           
 
             foreach (Animal animal in anis)
             {
-                Console.WriteLine(animal.ChipNumber);
+                Console.WriteLine("list anis2 "+animal.ChipNumber);
             }
 
 
         }
+        
         
     }
 }
